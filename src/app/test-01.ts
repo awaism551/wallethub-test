6d4c2e6beb63c6bd0af944256cae9d1955cabcfa
Muhammad Awais <muhammad.awais4@afiniti.com>
2022-10-16 14:03:25 +0500

    angular test-01

--- src/app/test-01.ts
+++ src/app/test-01.ts
@@ -5,33 +5,56 @@
  * * Both [monthly_payment] and [late_payment] should print in the template in currency format : $1,234
  */
 
-import { Component, Input,NgModule  } from '@angular/core';
+import { CommonModule } from "@angular/common";
+import { Component, NgModule, OnInit } from "@angular/core";
+import { FormsModule } from "@angular/forms";
 import { RouterModule } from "@angular/router";
 
 @Component({
-    selector : 'ng-app',
-    template : `<div>
-                    <h2>Loan Details</h2>
-                    <b>Monthly Payment:</b> {{monthly_payment}} <br/>
-                    <b>Late Payment Fee : {{late_payment}}</b> <br/>
-                </div>`
+  templateUrl: "./templates/test-01.html",
+  // selector : 'ng-app',
+  // template : `<div>
+  //                 <h2>Loan Details</h2>
+  //                 <b>Monthly Payment:</b> {{monthly_payment}} <br/>
+  //                 <b>Late Payment Fee : {{late_payment}}</b> <br/>
+  //             </div>`
 })
-export class Test01Component {
+export class Test01Component implements OnInit {
+  loan_amount: number = 1000;
+  monthly_payment: string | number;
+  late_payment: string | number;
 
-    loan_amount:number = 1000;
-    monthly_payment:number = 200;
-    late_payment = 10;
+  ngOnInit() {
+    this.updateValues();
+  }
+
+  loanAmountChanged(event) {
+    console.log("called", event.target.valueAsNumber);
+    this.loan_amount = event.target.valueAsNumber;
+    this.updateValues();
+  }
+  updateValues() {
+    if (this.loan_amount <= 0) {
+      this.monthly_payment = "N/A";
+      this.late_payment = "N/A";
+    } else {
+      this.monthly_payment = this.loan_amount * 0.02;
+      this.late_payment = this.monthly_payment * 0.05;
+    }
+  }
 }
 
 @NgModule({
-    imports : [
-        RouterModule.forChild([
-            {
-                path : "",
-                component : Test01Component
-            }
-        ])
-    ],
-    declarations : [Test01Component]
+  imports: [
+    RouterModule.forChild([
+      {
+        path: "",
+        component: Test01Component,
+      },
+    ]),
+    FormsModule,
+    CommonModule
+  ],
+  declarations: [Test01Component],
 })
 export class Test01Module {}
