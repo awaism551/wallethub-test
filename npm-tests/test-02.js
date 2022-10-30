5dbcf7e7cbd231c19ddd15b8a332317091b32018
Muhammad Awais <muhammad.awais4@afiniti.com>
2022-10-30 13:49:09 +0500

    node test 2

--- npm-tests/test-02.js
+++ npm-tests/test-02.js
@@ -7,9 +7,25 @@
 import https from "https";
 
 
-https.get('https://coderbyte.com/api/challenges/json/rest-get-simple', (resp) => {
+https
+.get('https://coderbyte.com/api/challenges/json/rest-get-simple', (resp) => {
     let data = "";
-    
+    // console.log(resp);
+    resp.on('data', function (chunk) {
+        data += chunk;
+    });
+    resp.on('end', function() {
+        // console.log('ended', JSON.parse(data));
+        let result = JSON.parse(data);
+        let resultToPrint = {};
+        result.hobbies.forEach((hobby,index) => {
+            resultToPrint[`ITEM ${index+1}`] = hobby;
+        });
+        console.log(resultToPrint);
+        console.log('resultToPrint');
+    });
     // parse json and print "hobbies" property as ITEM1, ITEM2,...
-    console.log();
 })
+.on('error', function(e) {
+    console.log("Got error: " + e.message);
+});
